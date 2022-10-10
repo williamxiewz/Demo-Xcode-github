981dc56e223e70f44e2fe952ce31259a03411fa2
williamxiewz <hyw540667209@gmail.com>
2022-10-10 11:15:29 +0800

    删除fastlane swift

--- fastlane/Appfile.swift
+++
@@ -1,7 +0,0 @@
-var appIdentifier: String { return "com.powechi.Demo" } // The bundle identifier of your app
-var appleID: String { return "[[APPLE_ID]]" } // Your Apple Developer Portal username
-
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
