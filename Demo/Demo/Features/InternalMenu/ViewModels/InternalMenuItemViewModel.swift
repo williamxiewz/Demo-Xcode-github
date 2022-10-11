5a05869ac90865f481de22c3820d6fc5edc2b7fe
williamxiewz <hyw540667209@gmail.com>
2022-10-11 11:12:56 +0800

    update

--- Demo/Demo/Features/InternalMenu/ViewModels/InternalMenuItemViewModel.swift
+++ Demo/Demo/Features/InternalMenu/ViewModels/InternalMenuItemViewModel.swift
@@ -7,12 +7,13 @@
 
 import Foundation
 
+// 内部菜单类型
 enum InternalMenuItemType: String {
     case description
     case featureToggle
     case actionTrigger
 }
-
+/// 内部菜单VM
 protocol InternalMenuItemViewModel {
     var type: InternalMenuItemType { get }
     var title: String { get }
